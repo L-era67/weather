fdf83cfe28b339e6ad46b7216a6234028db04053
L-era67 <boloroots47@gmail.com>
2025-05-12 16:11:49 +0800

    ehniih WEather

---
+++ src/components/backGround.jsx
@@ -0,0 +1,10 @@
+export const Background = () =>{
+    return(
+        <div className="relative w-[940px] h-[940px]  bg-amber-700 ">
+            <div className="absolute top-[400px] left-[400px]  w-[140px] h-[140px] border-1 border-white-500  rounded-full"></div>
+            <div className="absolute top-[300px] left-[300px]  w-[340px] h-[340px] border-1 border-white-500 rounded-full"></div>
+            <div className="absolute top-[200px] left-[200px]  w-[540px] h-[540px] border-1 border-white-500 rounded-full"></div>
+            <div className="relative top-[0] left-[0]  w-[940px] h-[940px] border-1 border-white-500 rounded-full"></div>
+        </div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import {Background} from "../components/backGround"
+
+
+export default function Home() {
+  return (
+    <div className="bg-black h-[100vh]">
+      <Background/>
+    </div>
+  );
+}
