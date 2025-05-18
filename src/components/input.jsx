6e3b97b314df0f947ddbffa5791f19c5e4324ba4
L-era67 <boloroots47@gmail.com>
2025-05-19 00:39:28 +0800

    buu med

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,25 +1,32 @@
 import { SearchIcon } from "lucide-react";
 
-export const Input = ({ getWeather,setInput }) => {
+export const Input = ({ getWeather,getInput, input, filteredCity, setInput}) => {
  
+const changeWeather = (value) =>{
+  setInput(value.split(",")[0]);
+  getWeather();
+}
 
-  const getInput = (event) =>{
-    setInput(event.target.value)
-  }
 
 
   return (
     <div className="absolute z-30 top-[110px] left-[40px] flex px-[24px] py-[16px] bg-white justify-start items-center gap-[16px] rounded-[48px] w-[567px] ">
       <SearchIcon className="text-black" />
       <input
+      value={input}
         type="text"
         placeholder="Search"
         className="text-[32px]   text-gray-500 font-bold bg-[#FFFFFF] outline-none"
         onChange={getInput}
       />
-      <button onClick={getWeather} className=" text-amber-300 ">
+      {filteredCity.map((city, index)=>(
+        <div key={index} onClick={()=> changeWeather(city)}>
+          {city}
+        </div>
+      ))}
+      {/* <button onClick={getWeather} className=" text-amber-300 ">
         GET weather
-      </button>
+      </button> */}
     </div>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,82 +3,92 @@ import { Input } from "../components/input";
 import { Nar } from "../components/Nar";
 import { Sar } from "../components/Sar";
 import { useEffect, useState } from "react";
+import { LucideFileInput } from "lucide-react";
 
 export default function Home() {
   const [weather, setWeather] = useState({});
   const [input, setInput] = useState("Ulaanbaatar");
-  const [cities, setCities] = useState([])
-
-
-
-  // const cityUrl = `https://api.api-ninjas.com/v1/city?name=${input}`;
-
-  // const getCity = async () => {
-  //   try {
-  //     const response = await fetch(cityUrl, {
-  //       headers: {
-  //         "X-Api-Key": process.env.NEXT_PUBLIC_CITY_API_KEY,
-  //       },
-  //     });
-
-  //     const data = await response.json();
-
-  //     return data;
-  //   } catch (error) {
-  //     console.log("CITY error:", error);
-  //   }
-  // };
+  const [cities, setCities] = useState([]);
+  const [filteredCity, setFilteredCity]=useState([]);
 
   const getWeather = async () => {
     try {
-      // const city = await getCity();
-      // console.log(city);
+      console.log("INPUT::", input);
       const response = await fetch(
-        // `https://api.openweathermap.org/data/2.5/weather?lat=${city[0].latitude}&lon=${city[0].longitude}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=metric`
-       `https://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${input}`
+        `https://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${input}`
       );
 
       const data = await response.json();
-
+      console.log("DATA:", data);
       setWeather(data);
     } catch (error) {
       console.log("WEATHER ALDAA:", error);
     }
   };
 
-  const getCities =async () =>{
+  // const getCities =async () =>{
+  //   try {
+  //      const response = await fetch (`https://countriesnow.space/api/v0.1/countries`);
+
+  //      const medeelel =await response.json(); // {error: , msg: , "data:" [127sh],} ene data-g awah heregtei
+
+  //      console.log(medeelel.data[0].cities[0]) //ingej baij yg ter hotiin ner garch irj baina
+  //      console.log("medeelel",medeelel)
+
+  //      const medeelelMassive = medeelel.data //odoo yg data:[{country}, {country:"ulsin ner" ,cities:[hotuudin ner baina] }]
+
+  //     const Country = medeelelMassive.filter((uls)=> uls.cities.find((hot)=> hot===input))
+
+  //      console.log("hot haih",Country)
+
+  //     //  const City = Country.find((city)=> console.log("CITY::",city.cities))
+
+  //     const City = Country[0].cities.find((city) => city===input)
+  //      console.log("CITY",City)
+
+  //      setInput(City);
+
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  const getCities = async () => {
     try {
-       const response = await fetch (`https://countriesnow.space/api/v0.1/countries`);
+      const response = await fetch(
+        `https://countriesnow.space/api/v0.1/countries`
+      );
 
-       const medeelel =await response.json(); // {error: , msg: , "data:" [],} ene data-g awah heregtei
+      const data = await response.json();
 
-       console.log(medeelel.data[0].cities[0]) //ingej baij yg ter hotiin ner garch irj baina
-       console.log(medeelel)
+      setCities(data.data);
+    } catch (error) {}
+  };
 
-       const medeelelMassive = medeelel.data //odoo yg data:[{country}, {country:"ulsin ner" ,cities:[hotuudin ner baina] }]
-
-      const Country = medeelelMassive.filter((uls)=> uls.cities.find((hot)=> hot==="Tokyo"))
-      
-       console.log( typeof Country)
-
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     getCities();
     getWeather();
-    console.log(getWeather())
-  }, [])
+    console.log(getWeather());
+  }, []);
 
-  console.log("weather data:", weather);
+  const getInput = (event) => {
+    setInput(event.target.value);
+
+    const citiesAndCountry = cities.flatMap((country) =>
+      country.cities.map((city) => `${city}, ${country.country}`)
+    );
+
+    const city = citiesAndCountry?.filter((item) =>
+      item.toLowerCase().startsWith(input.toLowerCase())).slice(0, 4);
+
+      setFilteredCity(city)
+  };
 
   return (
     <div className="flex w-[100wv] h-[100vh] ">
       <div className="w-[50%] bg-[#F3F4F6]   flex items-center">
         <div className="relative  w-[800px] h-[1200px] border-2 border-indigo-600 my-0 mx-auto ">
-          <Input getWeather={getWeather} setInput={setInput} />
+          <Input getWeather={getWeather} getInput={getInput} input={input} filteredCity={filteredCity} setInput={setInput}/>
           {/* <Input getWeather={getWeather}/> */}
           <img
             src="./zurag/nar1.png"
@@ -104,7 +114,7 @@ export default function Home() {
           />
 
           <div className="absolute z-10 w-[100%] h-[100%] flex justify-center items-center">
-            <Sar  weather={weather}/>
+            <Sar weather={weather} />
           </div>
         </div>
       </div>
